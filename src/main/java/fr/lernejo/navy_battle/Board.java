4eea924f0fdabad10e5abdbc1db111c08fb221c1
thomas.vondracek <thomas.vondracek@epita.fr>
2023-01-12 19:52:36 +0100

    Partie 6 - Update File Board

--- src/main/java/fr/lernejo/navy_battle/Board.java
+++ src/main/java/fr/lernejo/navy_battle/Board.java
@@ -1,38 +1,32 @@
 package fr.lernejo.navy_battle;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Board
-{
+class Board {
     enum State {FREE, FIRED, HIT}
     enum FireResult {MISS, HIT, SUNK}
 
-    private final State[][] cells;
-    private final List<Ship> ships = new ArrayList<>();
+    private State[][] cells = new State[10][10];
+    private List<Ship> ships = new ArrayList<>();
 
-    public Board() {
-        this.cells = new State[10][10];
-        for (int j = 0; j < 10; j++) {
-            for (int k = 0; k < 10; k++) {
-                this.cells[j][k] = State.FREE;
-            }
-        } placeShips();
+    Board() {
+        for (State[] row : cells) {
+            Arrays.fill(row, State.FREE);
+        }
+        ships.add(new Ship(0, 0, 5, Ship.Orientation.VERTICAL));
+        ships.add(new Ship(6, 9, 4, Ship.Orientation.HORIZONTAL));
+        ships.add(new Ship(3,5, 3, Ship.Orientation.HORIZONTAL));
+        ships.add(new Ship(6, 6, 3, Ship.Orientation.VERTICAL));
+        ships.add(new Ship(1, 1, 2, Ship.Orientation.VERTICAL));
     }
 
-    private void placeShips() {
-        this.ships.add(new Ship(0, 0, 5, Ship.Orientation.VERTICAL));
-        this.ships.add(new Ship(6, 9, 4, Ship.Orientation.HORIZONTAL));
-        this.ships.add(new Ship(3,5, 3, Ship.Orientation.HORIZONTAL));
-        this.ships.add(new Ship(6, 6, 3, Ship.Orientation.VERTICAL));
-        this.ships.add(new Ship(1, 1, 2, Ship.Orientation.VERTICAL));
-    }
-
-    public FireResult takeFireFromEnemy(BoardPosition p) {
-        for (Ship s: this.ships) {
+    FireResult takeFireFromEnemy(BoardPosition p) {
+        for (Ship s : ships) {
             if (s.shootAtShip(p)) {
                 if (s.isShipSunk()) {
-                    this.ships.remove(s);
+                    ships.remove(s);
                     return FireResult.SUNK;
                 }
                 return FireResult.HIT;
@@ -41,15 +35,18 @@ public class Board
         return FireResult.MISS;
     }
 
-    public boolean shipLeft() {
-        return !this.ships.isEmpty();
+    boolean shipLeft() {
+        return !ships.isEmpty();
     }
 
-    public void updateCell(int x, int y, boolean enemy) {
-        this.cells[x][y] = (enemy ? State.HIT : State.FIRED);
+    void updateCell(int x, int y, boolean enemy) {
+        cells[x][y] = enemy ? State.HIT : State.FIRED;
     }
 
-    public State getCellState(int x, int y) {
-        return this.cells[x][y];
+    State getCellState(int x, int y) {
+        return cells[x][y];
     }
 }
+
+
+
