889eac55274b28a45852276fe279c0cab0807d30
thomas.vondracek <thomas.vondracek@epita.fr>
2023-01-12 12:10:11 +0100

    Partie 6 - Update StartHandlerTest

--- src/test/java/fr/lernejo/navy_battle/StartHandlerTest.java
+++ src/test/java/fr/lernejo/navy_battle/StartHandlerTest.java
@@ -24,4 +24,15 @@ class StartHandlerTest {
         server.stop(0);
     }
 
+    //faire un autre test mais erreur 404
+    void start_handler_test_404() throws Exception {
+        var server = Launcher.Server(new GameState("http://localhost:1234"));
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder().uri(URI.create("http://localhost:1234/api/game/start")).POST(HttpRequest.BodyPublishers.ofString("{\"url\": \"http://localhost:1234\"}")).build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertThat(response.statusCode()).isEqualTo(404);
+        Assertions.assertThat(response.body()).isEqualTo("{\"id\": \"10\", \"url\": \"http://localhost:1234\", \"message\": \"May the best code win !\"}");
+        server.stop(0);
+    }
+
 }
