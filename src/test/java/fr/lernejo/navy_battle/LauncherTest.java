35f25c7292e72ca64c5e3ca4a1ea77708ea8560a
thomas.vondracek <thomas.vondracek@epita.fr>
2023-01-12 12:25:34 +0100

    Partie 6 - Update LauncherTest(FIX)

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -26,4 +26,28 @@ class LauncherTest {
 
     }
 
+    //vérifier que le serveur est bien lancé
+    @Test
+    void server_is_running() throws Exception {
+        var server = Launcher.Server(new GameState("http://localhost:1234"));
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder().uri(URI.create("http://localhost:1234/ping")).GET().build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertThat(response.statusCode()).isEqualTo(200);
+        Assertions.assertThat(response.body()).isEqualTo("OK");
+        server.stop(0);
+    }
+
+    //vérifie si args.length == 2 et si oui, Client(game, args) est appelé
+    @Test
+    void client_is_called() throws Exception {
+        var server = Launcher.Server(new GameState("http://localhost:1234"));
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder().uri(URI.create("http://localhost:1234/api/game/start")).POST(HttpRequest.BodyPublishers.ofString("{\"url\": \"http://localhost:1234\"}")).build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertThat(response.statusCode()).isEqualTo(202);
+        Assertions.assertThat(response.body()).isEqualTo("{\"id\": \"10\", \"url\": \"http://localhost:1234\", \"message\": \"May the best code win !\"}");
+        server.stop(0);
+    }
+
 }
