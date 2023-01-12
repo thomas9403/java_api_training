380c746e338b163f7ce1ad1d855d2e2204747a46
thomas.vondracek <thomas.vondracek@epita.fr>
2023-01-12 21:10:53 +0100

    Partie 6 - Add Test To File LauncherTest (Code Coverage)

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -26,4 +26,49 @@ class LauncherTest {
         });
     }
 
+
+    //public static void main(String[] args) {
+        //if (args.length == 0 || args.length > 2) {
+          //  System.out.println("Listen"); return;
+        //}
+        //GameState game = new GameState("http://localhost:" + Integer.parseInt(args[0]));
+        //Server(game);
+        //if (args.length == 2) Client(game, args);
+        //while (!game.is_game_over()) {
+            //if (game.get_turn()) FireProcedure(game);
+           // try { Thread.sleep(2500); }
+         // }
+     //   System.out.println("Game is Over!");
+   // }
+
+    @Test
+    //tester GameState game = new GameState("http://localhost:" + Integer.parseInt(args[0]));
+    void test_gamestate(){
+        GameState game = new GameState("http://localhost:8080");
+        Assertions.assertThat(game.getPort()).isEqualTo(8080);
+    }
+
+    @Test
+    //tester Server(game);
+    void test_server(){
+        GameState game = new GameState("http://localhost:8080");
+        Assertions.assertThat(Launcher.Server(game)).isNotNull();
+    }
+
+    @Test
+    //tester FireProcedure(game);
+    void test_fireprocedure(){
+        GameState game = new GameState("http://localhost:8080");
+        Launcher.Client(game, new String[]{"8080", "http://localhost:8081"});
+        Launcher.FireProcedure(game);
+        Assertions.assertThat(game.getPosState(0,0)).isEqualTo(Board.State.FREE);
+    }
+
+    @Test
+    //tester RandomPos(game);
+    void test_randompos(){
+        GameState game = new GameState("http://localhost:8080");
+        Assertions.assertThat(Launcher.RandomPos(game)).isNotNull();
+    }
+
 }
